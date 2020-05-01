345524fd154138ac95fbc9d2307f25b89ec4f500
eosfelipe <escobedo.felipe@hotmail.com>
2020-05-01 17:52:36 -0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+if(process.env.NODE_ENV === 'production') {
+    require('dotenv').config();
+}
+console.log(process.env.NODE_ENV);
+
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+
+//initialization
+const app = express();
+require('./database');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//middlewares
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename(req, file, cb) {
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+});
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.use('/api/books', require('./routes/books'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//start the server
+app.listen(app.get('port'), () => {
+    console.log('>>>SERVER ON PORT', app.get('port'));
+})
